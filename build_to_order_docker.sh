076e2c03c6f12f869d620e5bc3655b236ca4230e
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-02-08 09:19:37 -0600

    playing around with docker

--- build_to_order_docker.sh
+++ build_to_order_docker.sh
@@ -1,4 +1,4 @@
-docker run -it --rm --name app -v "$PWD":/Lis_Py python:3 python Lis_Py/lis.py || docker start -ia app
+docker run -it --rm --name lispy -v "$PWD":/Lis_Py python:3 python Lis_Py/lis.py || docker start -ia lispy
 
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
