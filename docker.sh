5ba123c367ab783caea0bba320b404767e2df7a7
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-02-11 23:42:28 -0600

    added instructions on connecting to docker from WSL

--- docker.sh
+++ docker.sh
@@ -1,2 +1,6 @@
 docker build -t lis.py .
 docker run -it --rm --name lispy-running lis.py
+
+# https://blog.jayway.com/2017/04/19/running-docker-on-bash-on-windows/
+# https://stackoverflow.com/questions/48047810/cannot-connect-to-the-docker-daemon-on-bash-on-ubuntu-windows
+# https://stackoverflow.com/questions/26424338/docker-daemon-config-file-on-boot2docker-docker-machine-docker-toolbox/26781047#26781047
